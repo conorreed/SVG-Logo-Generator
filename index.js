fea18a1d2460e955dd98a124436af16208b26628
Conor <conor.reed@gmail.com>
2023-10-16 17:15:23 -0500

    added a logoutility.js file that validates the color choicesa and creates a class constructor of the inputs

--- index.js
+++ index.js
@@ -12,3 +12,8 @@
 // AND the output text "Generated logo.svg" is printed in the command line
 // WHEN I open the `logo.svg` file in a browser
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered
+
+const { promptUserForLogoInfo } = require('./src/logoUtility');
+
+// Start the application by prompting the user for logo information
+promptUserForLogoInfo();
